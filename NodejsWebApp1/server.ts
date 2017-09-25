9734f15ab3a8f1545b6599f72eaf2ba09a9ec380
Atul Malaviya <azooinmyluggage@users.noreply.github.com>
2017-09-26 01:55:47 +0530

    Update server.ts

--- NodejsWebApp1/server.ts
+++ NodejsWebApp1/server.ts
@@ -1,6 +1,7 @@
 ﻿import http = require('http');
 import os = require('os');
 var port = process.env.port || 1337
+var port = process.env.NODE_ENV || 1337
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World app served from environment: ' + process.env.NODE_ENV);
