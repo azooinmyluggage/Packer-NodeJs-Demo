c26a36c9de05d64468f29059c474729f85a02ed1
Atul Malaviya <azooinmyluggage@users.noreply.github.com>
2017-09-29 15:17:04 +0530

    Update puppetmanifest.pp

--- NodejsWebApp1/Deploy/ubuntu/puppetmanifest.pp
+++ NodejsWebApp1/Deploy/ubuntu/puppetmanifest.pp
@@ -3,15 +3,5 @@ file {'/tmp/test1':
   content => "Hello World!",
 }
 
-file {'/tmp/test2':
-  ensure => directory,
-  mode   => 0644,
-}
-
-file {'/tmp/test3':
-  ensure => link,
-  target => '/tmp/test1',
-}
-
 notify {"I'm notifying you.":} # Whitespace is fungible, remember.
 notify {"So am I!":}
